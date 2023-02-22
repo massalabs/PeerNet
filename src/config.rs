5e7b18f471228016f797bfb7ad1c9cedbac9bdce
AurelienFT <aurelien.foucault@epitech.eu>
2023-02-22 01:43:19 +0100

    Remove old architecture

--- src/config.rs
+++
@@ -1,9 +0,0 @@
-use std::{net::SocketAddr, collections::HashMap};
-
-use crate::transport::TransportType;
-
-pub struct PeerNetConfiguration {
-    pub transports: HashMap<TransportType, SocketAddr>,
-    // Number of peers we want to have
-    pub max_peers: usize,
-}
--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-mod connection_listener;
-
-pub mod config;
-pub mod transport;
-pub mod network_manager;
-pub mod peer;
