296a0f6f1abc40313b06c8d82b1cf64215518c62
Litchi Pi (Tim) <litchi.pi@protonmail.com>
2023-04-27 16:21:30 +0200

    Use named threads to improve debugging (#36)
    * Use named threads to improve debugging
    
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- src/transports/tcp.rs
+++ src/transports/tcp.rs
@@ -123,7 +123,9 @@ impl Transport for TcpTransport {
         let mut events = Events::with_capacity(128);
         let waker = Waker::new(poll.registry(), STOP_LISTENER)
             .map_err(|err| TcpError::InitListener.wrap().new("waker new", err, None))?;
-        let listener_handle: JoinHandle<PeerNetResult<()>> = std::thread::spawn({
+        let listener_handle: JoinHandle<PeerNetResult<()>> = std::thread::Builder::new()
+            .name(format!("tcp_listener_handle_{:?}", address))
+            .spawn({
             let active_connections = self.active_connections.clone();
             let reject_same_ip_addr = self.features.reject_same_ip_addr;
             let peer_stop_rx = self.peer_stop_rx.clone();
@@ -213,7 +215,7 @@ impl Transport for TcpTransport {
                     }
                 }
             }
-        });
+        }).expect("Failed to spawn thread tcp_listener_handle");
         {
             let mut active_connections = self.active_connections.write();
             active_connections
@@ -234,50 +236,53 @@ impl Transport for TcpTransport {
         handshake_handler: T,
     ) -> PeerNetResult<JoinHandle<PeerNetResult<()>>> {
         let peer_stop_rx = self.peer_stop_rx.clone();
-        Ok(std::thread::spawn({
-            let active_connections = self.active_connections.clone();
-            let wg = self.out_connection_attempts.clone();
-            move || {
-                let stream = TcpStream::connect_timeout(&address, timeout).map_err(|err| {
-                    TcpError::ConnectionError.wrap().new(
-                        "try_connect stream connect",
-                        err,
-                        Some(format!("address: {}, timeout: {:?}", address, timeout)),
-                    )
-                })?;
-                let stream = Limiter::new(stream, RATE_LIMIT, Duration::from_secs(1));
-                println!("Connected to {}", address);
+        Ok(std::thread::Builder::new()
+            .name(format!("tcp_try_connect_{:?}", address))
+            .spawn({
+                let active_connections = self.active_connections.clone();
+                let wg = self.out_connection_attempts.clone();
+                move || {
+                    let stream = TcpStream::connect_timeout(&address, timeout).map_err(|err| {
+                        TcpError::ConnectionError.wrap().new(
+                            "try_connect stream connect",
+                            err,
+                            Some(format!("address: {}, timeout: {:?}", address, timeout)),
+                        )
+                    })?;
+                    let stream = Limiter::new(stream, RATE_LIMIT, Duration::from_secs(1));
+                    println!("Connected to {}", address);
 
-                {
-                    let mut active_connections = active_connections.write();
-                    if active_connections.nb_out_connections
-                        < active_connections.max_out_connections
                     {
-                        active_connections.nb_out_connections += 1;
-                    } else {
-                        return Err(PeerNetError::BoundReached.error(
-                            "tcp try_connect max_out_conn",
-                            Some(format!(
-                                "max: {}, nb: {}",
-                                active_connections.max_out_connections,
-                                active_connections.nb_out_connections
-                            )),
-                        ))?;
+                        let mut active_connections = active_connections.write();
+                        if active_connections.nb_out_connections
+                            < active_connections.max_out_connections
+                        {
+                            active_connections.nb_out_connections += 1;
+                        } else {
+                            return Err(PeerNetError::BoundReached.error(
+                                "tcp try_connect max_out_conn",
+                                Some(format!(
+                                    "max: {}, nb: {}",
+                                    active_connections.max_out_connections,
+                                    active_connections.nb_out_connections
+                                )),
+                            ))?;
+                        }
                     }
+                    new_peer(
+                        self_keypair.clone(),
+                        Endpoint::Tcp(TcpEndpoint { address, stream }),
+                        handshake_handler.clone(),
+                        message_handler.clone(),
+                        active_connections.clone(),
+                        peer_stop_rx,
+                        ConnectionType::OUT,
+                    );
+                    drop(wg);
+                    Ok(())
                 }
-                new_peer(
-                    self_keypair.clone(),
-                    Endpoint::Tcp(TcpEndpoint { address, stream }),
-                    handshake_handler.clone(),
-                    message_handler.clone(),
-                    active_connections.clone(),
-                    peer_stop_rx,
-                    ConnectionType::OUT,
-                );
-                drop(wg);
-                Ok(())
-            }
-        }))
+            })
+            .expect("Failed to spawn thread tcp_try_connect"))
     }
 
     fn stop_listener(&mut self, address: SocketAddr) -> PeerNetResult<()> {
