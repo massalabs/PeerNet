373d724964b0817a370153e51d0236dee49c440c
AurelienFT <aurelien.foucault@epitech.eu>
2023-04-28 11:51:22 +0200

    Change type exports

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,5 @@
-pub type Signature = massa_signature::Signature;
-pub type PublicKey = massa_signature::PublicKey;
-pub type KeyPair = massa_signature::KeyPair;
-pub type Hash = massa_hash::Hash;
+pub use massa_signature::Signature;
+pub use massa_signature::PublicKey;
+pub use massa_signature::KeyPair;
+pub use massa_hash::Hash;
 pub const PUBLIC_KEY_SIZE_BYTES: usize = massa_signature::PUBLIC_KEY_SIZE_BYTES;
